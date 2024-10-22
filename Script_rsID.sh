7877c39cc752d21448a01154197f64a94ff6743a
Adriano Weber <adriano.weber@etu.unige.ch>
2024-10-22 11:48:24 +0200

    First operational commit

--- Script_rsID.sh
+++ Script_rsID.sh
@@ -1,72 +1,153 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <chemin_du_fichier_zip>"
+###USAGE
+usage(){
+  echo "Usage: $0 -options <path_to_file.zip>"
+  echo "OPTIONS:"
+  echo "-o: Where to write the output file(s)."
+  echo "-s: If provided then only the final annotated file will be kept"
+  echo "-d: Where is the database if it's a local use."
+  echo "-h: Help of the script."
+}
+
+if [ "$#" -eq 0 ]; then
+  echo "No parameter provided."
+  usage
   exit 1
 fi
 
-ZIP_FILE="$1"  #Ici cela doit être la direction du fichier zip
-OUTPUT_DIR="${2:-.}" #Cela dit de prendre la deuxième option si fournie, sinon ce sera la direction actuelle
-BCF_LIST_FILE="bcf_files.txt"
+###Initialisation of parameters
+SILENT=0
 
-if ! command -v bcftools &> /dev/null
-then
-    echo "bcftools n'est pas installé. Veuillez l'installer avant de continuer."
+if [ -z "$OUTPUT_DIR" ]; then
+    OUTPUT_DIR=.
+fi
+
+if [ -z "$DB_DIR" ]; then
+    DB_DIR=.
+    #DB_DIR=/mnt/c/Users/adria/Documents/Unige/Masters_project/Data_PacBio/clinvar.vcf.gz
+fi
+
+###OPTIONS setting
+while getopts ":ho:sd:" opt; do
+    case "${opt}" in
+          #help flag
+      h)
+        usage
+        exit
+        ;;
+      o)
+        OUTPUT_DIR="${OPTARG}"
+        # Check if the direction exits and create it if not
+        if [ ! -d "$OUTPUT_DIR" ]; then
+            mkdir -p "$OUTPUT_DIR"  # Uses -p to create upper level direction
+        fi
+        ;;
+      s)
+        SILENT=1
+        ;;
+      d)
+        DB_DIR="${OPTARG}"
+        ;;
+      #if another flag is used, error
+      *)
+        usage
+        ;;
+    esac
+done
+
+shift $((OPTIND - 1)) ##Cette ligne est très sus
+
+###Setting of variables
+ZIP_FILE="$1"  #Here it register the first parameter as the file to unzip
+if [ ! -f "$ZIP_FILE" ]; then
+    echo "Error with $ZIP_FILE no such file."
+    usage
     exit 1
 fi
 
-echo "Décompression du fichier zip..."
+BCF_LIST_FILE="bcf_files.txt"
+tmp_files=()
+
+####Check if bcftools is installed
+if ! command -v bcftools &> /dev/null; then
+    echo "bcftools is not installed. Install it to proceed."
+    exit 1
+fi
+
+###Opening of the zip file
+echo "Unzipping input file..."
 unzip "$ZIP_FILE" -d "$OUTPUT_DIR"
 
-echo "Recherche des fichiers BCF..."
-find "$OUTPUT_DIR" -name "*.gz" > "$BCF_LIST_FILE" #Ici modifier pour pouvoir utiliser des bcf
+echo "Searching BCF files..."
+find "$OUTPUT_DIR" -name "*.gz" > "$OUTPUT_DIR/$BCF_LIST_FILE" #This line read only the zipped vcf, it could be useful to extend to bcf files too
+tmp_files+=( "$(find "$OUTPUT_DIR" -name "*.gz")" )
+tmp_files+=( "$OUTPUT_DIR/$BCF_LIST_FILE" )
 
-# Indexation des fichiers BCF
-echo "Indexation des fichiers BCF..."
+###Indexing BCF files
+echo "Indexing BCF files..."
 while IFS= read -r BCF_FILE; do
-    echo "Indexation de $BCF_FILE..."
-    bcftools index "$BCF_FILE"
-    if [ $? -ne 0 ]; then
-        echo "Erreur lors de l'indexation de $BCF_FILE"
+    echo "Indexation of $BCF_FILE..."
+    if bcftools index "$BCF_FILE"; then
+        tmp_files+=( "$BCF_FILE" "$BCF_FILE.csi" )
+    else
+        echo "Error during indexing $BCF_FILE"
+        rm -f "${tmp_files[@]}"
         exit 1
     fi
 done < "$OUTPUT_DIR/$BCF_LIST_FILE"
 
-if [ ! -s "$BCF_LIST_FILE" ]; then
-  echo "Aucun fichier BCF trouvé."
+if [ ! -s "$OUTPUT_DIR/$BCF_LIST_FILE" ]; then
+  echo "No BCF file was found."
+  rm -f "${tmp_files[@]}"
+  echo "All temporary files have been deleted."
   exit 1
 fi
 
-echo "Fusion des fichiers BCF avec bcftools..."
-bcftools merge -o merged_output.bcf -O b -l "$BCF_LIST_FILE"
-if [ $? -eq 0 ]; then
-  echo "Fusion terminée avec succès ! Le fichier final est 'merged_output.bcf'."
+###Merging of BCF files
+echo "Merging the BCF files using bcftools..."
+if bcftools merge -o "$OUTPUT_DIR/merged_output.bcf" -O b -l "$OUTPUT_DIR/$BCF_LIST_FILE"; then
+  tmp_files+=( "$OUTPUT_DIR/merged_output.bcf" )
+  echo "Success ! The merged file was saved as 'merged_output.bcf'."
+  tabix "$OUTPUT_DIR/merged_output.bcf"
+  tmp_files+=("$OUTPUT_DIR/merged_output.bcf.csi")
 else
-  echo "Erreur lors de la fusion des fichiers BCF."
+  echo "Error during the merger of BCF files."
+  rm -f "${tmp_files[@]}"
+  echo "All temporary files have been deleted."
   exit 1
 fi
-tabix merged_output.bcf
 
-###Changement de la colonne CHROM pour correspondre à clinvar
-echo "Réecriture de la colonne CHROM...."
-bcftools annotate --rename-chrs <(echo -e "chr1\t1\nchr2\t2\nchr3\t3\nchr4\t4\nchr5\t5\nchr6\t6\nchr7\t7\nchr8\t8\nchr9\t9\nchr10\t10\nchr11\t11\nchr12\t12\nchr13\t13\nchr14\t14\nchr15\t15\nchr16\t16\nchr17\t17\nchr18\t18\nchr19\t19\nchr20\t20\nchr21\t21\nchr22\t22\nchrX\tX\nchrY\tY\nchrM\tM") merged_output.bcf -o rewrited_output.bcf -O b
-if [ $? -eq 0 ]; then
-  echo "Réécriture réussie! Le fichier réécrit a été enregistré sous 'rewrited_output.bcf'"
+###Rewriting of CHROM column
+echo "Rewriting CHROM column to standarize it...."
+if bcftools annotate --rename-chrs <(echo -e "chr1\t1\nchr2\t2\nchr3\t3\nchr4\t4\nchr5\t5\nchr6\t6\nchr7\t7\nchr8\t8\nchr9\t9\nchr10\t10\nchr11\t11\nchr12\t12\nchr13\t13\nchr14\t14\nchr15\t15\nchr16\t16\nchr17\t17\nchr18\t18\nchr19\t19\nchr20\t20\nchr21\t21\nchr22\t22\nchrX\tX\nchrY\tY\nchrM\tM") "$OUTPUT_DIR/merged_output.bcf" -o "$OUTPUT_DIR/rewrited_output.bcf" -O b; then
+  tmp_files+=( "$OUTPUT_DIR/rewrited_output.bcf" )
+  echo "Success! The rewrited files was saved as 'rewrited_output.bcf'"
+  tabix "$OUTPUT_DIR/rewrited_output.bcf"
+  tmp_files+=("$OUTPUT_DIR/rewrited_output.bcf.csi")
 else
-  echo "Erreur lors de la réécriture du fichier bcf."
+  echo "Error during the writing of BCF file."
+  rm -f "${tmp_files[@]}"
+  echo "All temporary files have been deleted."
   exit 1
 fi
-tabix rewrited_output.bcf
 
-###Maintenant l'annotation du bcf avec des ID
-echo "Usage de clinvar.vcf.gz update de 2024-09-18. https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/"
-echo "Ajout des IDs selon ClinVar...."
 
-bcftools annotate -a /mnt/c/Users/adria/Documents/Unige/Masters_project/Data_PacBio/clinvar.vcf.gz -c ID -o annotate_output.bcf -O b rewrited_output.bcf
-if [ $? -eq 0 ]; then
-  echo "Annotation réussie! Le fichier annoté a été enregistré sous 'annotate_output.bcf'"
+###Annotation of BCF
+echo "Use of 'clinvar.vcf.gz' 2024-09-18 update from https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/"
+echo "Adding IDs with ClinVar...."
+
+if bcftools annotate -a "$DB_DIR" -c ID -o "$OUTPUT_DIR/annotate_output.bcf" -O b "$OUTPUT_DIR/rewrited_output.bcf"; then
+  echo "Success! The annotated file was saved as 'annotate_output.bcf'"
+  tabix "$OUTPUT_DIR/annotate_output.bcf"
 else
-  echo "Erreur lors de l'annotation du fichier bcf."
+  echo "Error during the annotation of BCF file. Check access to the database."
+  rm -f "${tmp_files[@]}"
+  echo "All temporary files have been deleted."
   exit 1
 fi
 
-tabix annotate_output.bcf
+if [ "$SILENT" -eq 1 ]; then
+  echo "Destruction of temporary files..."
+  rm -f "${tmp_files[@]}"
+  echo "All temporary files have been deleted."
+fi
